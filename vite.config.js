341af596f7ce16700873c8d3e0658603238dbdf6
Arslan-the-Developer <callmearslanahmed@gmail.com>
2025-04-14 22:14:42 +0500

    First Commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import dotenv from 'dotenv'
+
+
+dotenv.config();
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base : process.env.VITE_BASE_URL || '/my_first_portfolio',
+})
