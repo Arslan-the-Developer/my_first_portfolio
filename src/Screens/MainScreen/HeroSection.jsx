a5d43b64cc1c7e8929ad5ea88182b2f835378578
Arslan-the-Developer <callmearslanahmed@gmail.com>
2025-05-27 16:22:00 +0500

    Updated HeroSection

--- src/Screens/MainScreen/HeroSection.jsx
+++ src/Screens/MainScreen/HeroSection.jsx
@@ -14,7 +14,7 @@ function HeroSection() {
 
   return (
 
-    <section className='w-full h-screen bg-dull flex flex-col items-center  justify-center'>
+    <section className='w-full bg-dull flex flex-col items-center  justify-center' style={{height : "100svh"}}>
         <GridDistortion
         imageSrc="https://images.unsplash.com/photo-1738831920727-73e17adc5b87?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         grid={20}
