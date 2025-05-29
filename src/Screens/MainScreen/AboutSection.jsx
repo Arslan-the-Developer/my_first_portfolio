387e700ff07be61cb18aa051820f153073a54b86
Arslan-the-Developer <callmearslanahmed@gmail.com>
2025-05-29 11:49:19 +0500

    Improved AboutSection

--- src/Screens/MainScreen/AboutSection.jsx
+++ src/Screens/MainScreen/AboutSection.jsx
@@ -29,12 +29,12 @@ function AboutSection() {
           <div className='w-full flex flex-col items-center justify-center content-center transition-all mt-15'>
 
             <h3 className='text-white w-full font-poppins font-thin italic text-center' style={{lineHeight : "1.4", fontSize : "calc(1.5rem + 1.5vw)"}}>
-              <span className='text-primary font-futura' style={{fontSize : "calc(2rem + 1.5vw)"}}>"</span> <span className='opacity-70' style={{textShadow : "0 0 10px #ffffff"}}>Infinite Loops of Creativity - <br /> Zero Bugs of Doubt</span> <span className='text-primary font-futura' style={{fontSize : "calc(2rem + 1.5vw)"}}>"</span>
+              <span className='text-primary font-futura' style={{fontSize : "calc(2rem + 1.5vw)"}}>"</span> <span className='opacity-70' style={{textShadow : "0 0 10px #ffffff"}}>Infinite Loops of Creativity - <br /> Zero Bugs of Doubt</span> <span className='text-primary font-futura' style={{fontSize : "calc(1.6rem + 1.6vw)"}}>"</span>
             </h3>
 
             <span className='w-40 h-[1px] bg-primary mt-15'></span>
 
-            <p className='w-155 max-sm:w-98 max-md:w-120 text-center text-white font-poppins tracking-wide mt-15 font-extralight' style={{lineHeight : 1.9, fontSize : "calc(0.8rem + 0.7vw)"}}>Hey There! I am Arslan Ahmad. I am a Full Stack Web Developer & <span className='px-2 font-normal bg-primary text-dull rounded-sm'>API Expert</span>. I Unleash The Power of -- Programming with over <span className='bg-primary font-normal px-2 rounded-sm text-dull'>3+ Years</span> of strong command on it & deliver secure web solutions.</p>
+            <p className='w-155 max-sm:w-88 max-md:w-120 text-center text-white font-poppins tracking-wide mt-15 font-extralight' style={{lineHeight : 1.9, fontSize : "calc(0.8rem + 0.7vw)"}}>Hey There! I am Arslan Ahmad. I am a Full Stack Web Developer & <span className='px-2 font-normal bg-primary text-dull rounded-sm'>API Expert</span>. I Unleash The Power of -- Programming with over <span className='bg-primary font-normal px-2 rounded-sm text-dull'>3+ Years</span> of strong command on it & deliver secure web solutions.</p>
 
           </div>
 
